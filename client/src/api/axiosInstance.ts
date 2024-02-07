797397fd7b752b827c7661cee6be635fccf9ea83
sumy77 <sumitmaddy23@gmail.com>
2024-02-07 19:48:30 +0530

    Revert "Restructured folders and created docker build on the fly with docker compose"
    This reverts commit db01b61920635c7004be97e15be35e47af393406.
    Reverted commit

--- client/src/api/axiosInstance.ts
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const axiosInstance = axios.create({
-    baseURL: "https://pokeapi.co/api/v2/",
-    timeout: 5000
-});
-
-export default axiosInstance;
--- client/src/api/localInstance.ts
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const localInstance = axios.create({
-    baseURL: "http://localhost:8888",
-    timeout: 5000
-})
-
-export default localInstance;
